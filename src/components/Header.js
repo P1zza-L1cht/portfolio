7c4ecf8544a5c5d63f4df03a8b66eceacf4fca89
P1zza-L1cht <haruki21corder@gmail.com>
2022-06-03 16:39:08 +0900

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import '../styles/Header.scss';
 
 export default function Header() {
   return(
     <header>
-      Header
+      <div className="logo"><h1>Portfolio</h1></div>
+      <ul className="navigation">
+        <li className="nav-btn link"><Link to='/'>ホーム<br/><p className="sub-title">Home</p></Link></li>
+        <li className="nav-btn link"><Link to='/works'>作品<br/><p className="sub-title">works</p></Link></li>
+        <li className="nav-btn link"><Link to='/experience'>経歴<br/><p className="sub-title">Experience</p></Link></li>
+        <li className="nav-btn link"><Link to='/contact' >お問い合わせ<br/><p className="sub-title">Contact</p></Link></li>
+      </ul>
     </header>
   );
 }
