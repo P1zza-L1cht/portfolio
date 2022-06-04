a402e81efb632cad6223c74360661504614d652a
P1zza-L1cht <haruki21corder@gmail.com>
2022-06-05 01:23:25 +0900

    subpage media

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import '../styles/Header.scss';
+import OnBtn from '../images/decoration/toggle1_on.png';
+import OffBtn from "../images/decoration/toggle1_off.png";
 
 export default function Header() {
+  const [isToggle, setIsToggle] = useState(false);
+  var toggleMark;
+  const location = useLocation();
+
+
+  useEffect(() => {
+    setIsToggle(false);
+  }, [location]);
+
+  if (isToggle) {
+    toggleMark = (<img src={OnBtn} alt="toggleOn" />);
+  } else {
+    toggleMark = (<img src={OffBtn} alt="toggleOff" />)
+  }
+
   return(
     <header>
       <div className="logo"><h1>Haruki Yui</h1></div>
-      <ul className="navigation">
+      <div
+        className="toggle-btn"
+        onClick={() => {
+          setIsToggle((prev) => !prev);
+        }}
+      >
+        {toggleMark}
+      </div>
+      <ul className="navigation" id={ isToggle ? "open" : "close"}>
         <li className="nav-btn link"><Link to='/'>ホーム<br/><p className="sub-title">Home</p></Link></li>
         <li className="nav-btn link"><Link to='/works'>作品<br/><p className="sub-title">works</p></Link></li>
         <li className="nav-btn link"><Link to='/experience'>経歴<br/><p className="sub-title">Experience</p></Link></li>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,22 +3,51 @@ import emailjs from 'emailjs-com';
 import '../styles/Contact.scss';
 
 export default function Contact() {
-  const [isSubmitted, setIsSubmitted] = useState(false);
+  const {isSubmitted, setIsSubmitted} = useState(false);
+  const {name, setName} = useState('');
+  const {email, setEmail} = useState('');
+  const {message, setMessage} = useState('');
+  const [errorMessages, setErrorMessage] = useState([]);
   const form = useRef();
   var content;
+
+  const handleName = (event) => {
+    setName(event.target.value);
+  }
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+  }
+
+  const handleMessage = (event) => {
+    setMessage(event.target.value);
+  }
 
   const handleSubmit = () => {
     setIsSubmitted(true);
   };
 
   const sendEmail = (e) => {
-    e.preventDefault();
+    if (name !== '' || email !== '' || message !== '' ){
+      e.preventDefault();
 
-    emailjs.sendForm('service_pe7sgfz', 'template_qs9zwjr', form.current, 'k8BQF6HD_FzXSUPJN').then(() => {
+      emailjs.sendForm('service_pe7sgfz', 'template_qs9zwjr', form.current, 'k8BQF6HD_FzXSUPJN').then(() => {
       handleSubmit();
     }, (error) => {
       console.log(error.text);
     });
+    }
+    setErrorMessage([]);
+    if (name === '') {
+      setErrorMessage({mes: '名前が入っていません'})
+    }
+    if (email === '') {
+      setErrorMessage(...errorMessages, {mes: 'メールアドレスがありません'})
+    }
+    if (message === '') {
+      setErrorMessage(...errorMessages, {mes: '内容がありません'})
+    }
+
   };
 
   if (isSubmitted === true) {
@@ -30,11 +59,16 @@ export default function Contact() {
   } else {
     content = (<div className="mail-container">
     <h3>お問い合わせフォーム</h3>
+    <div className="error-message">
+      {errorMessages.map((error) => {
+        return <p>{error.mes}</p>
+      })}
+    </div>
     <form onSubmit={sendEmail} ref={form} className="form">
-      <input type="text" className="form-control" placeholder="名前" name="name" />
-      <input type="email" className="form-control" placeholder="メールアドレス" name="email" />
+      <input type="text" className="form-control" placeholder="名前" name="name" onChange={(event) => {handleName(event)}} />
+      <input type="email" className="form-control" placeholder="メールアドレス" name="email" onChange={(event) => {handleEmail(event)}} />
       <p className="form-letter">内容</p>
-      <textarea className="form-control" cols="30" rows="8" name="message"></textarea>
+      <textarea className="form-control" cols="30" rows="8" name="message" onChange={(event) => {handleMessage(event)}}></textarea>
       <input type="submit" value="送信"></input>
     </form>
   </div>)
